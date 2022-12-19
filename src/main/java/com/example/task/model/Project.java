48cab1943f36663837dd547bdc3072e89619dfdc
DESKTOP-NTT999I\User <iamimrancse@gmail.com>
2022-12-19 23:13:59 +0600

    Crud for project

--- src/main/java/com/example/task/model/Project.java
+++ src/main/java/com/example/task/model/Project.java
@@ -2,12 +2,20 @@ package com.example.task.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "project")
 public class Project {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private String projectName;
-    private LocalDateTime springStartDate;
-    private LocalDateTime springEndDate;
+    private LocalDate sprintStartDate;
+    private LocalDate sprintEndDate;
 }
