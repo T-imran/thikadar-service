54eb125521619e58aa414fb946eddbef1c68e7d2
T-imran <iamimrancse@gmail.com>
2025-08-06 10:42:19 +0600

    Fix the Cors issue update the pom file and changed the application config file to yml.

--- src/main/java/com/mintidea/thikadar/module/auth/config/CORSFilter.java
+++ src/main/java/com/mintidea/thikadar/module/auth/config/CORSFilter.java
@@ -1,8 +1,8 @@
 package com.mintidea.thikadar.module.auth.config;
 
+import jakarta.servlet.*;
 import org.springframework.stereotype.Component;
 
-import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -23,7 +23,18 @@ public class CORSFilter implements Filter {
         filterChain.doFilter(servletRequest, res);
     }
 
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        HttpServletResponse res = (HttpServletResponse) response;
+//        res.setHeader("Access-Control-Allow-Origin", "*");
+//        res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+//        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//        res.setHeader("Access-Control-Max-Age", "3600");
+//        chain.doFilter(request, response);
+//    }
+
     public void destroy() {
+
     }
 
 }
